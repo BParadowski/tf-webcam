b86d423f119532787d1b75f0f4b54d8ac5875796
BParadowski <bartoszparadowski01@gmail.com>
2024-05-24 09:56:11 +0200

    Get basic skeleton in

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const status = document.getElementById("status");
-if (status) {
-  status.innerText = "Loaded TensorFlow.js - version: " + tf.version.tfjs;
+const statusMessage = document.getElementById("status");
+if (statusMessage) {
+  statusMessage.innerText =
+    "Loaded TensorFlow.js - version: " + tf.version.tfjs;
 }
+
+const video = document.getElementById("webcam");
+const liveView = document.getElementById("liveView");
+const demosSection = document.getElementById("demos");
+const enableWebcamButton = document.getElementById("webcamButton");
